bb4569a30ad460b2eae99a6ef0168d6a757009d0
ChangAn1988 <alstonbrett@gmail.com>
2016-11-20 19:15:32 +0800

    一個看每個狀態時會呼叫哪個方法的app

--- ShowActiveInLabel/AppDelegate.swift
+++ ShowActiveInLabel/AppDelegate.swift
@@ -16,29 +16,38 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        print("application第一次進入app時出現")
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        
+        print("applicationWillResignActive當app浮起來準備進入背景時出現")
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        
+        
+        print("applicationDidEnterBackground程式已經完全進入背景時出現")
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        print("applicationWillEnterForeground當程式由背景狀態重新回到app前景時出現")
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+        print("applicationDidBecomeActive剛開啟app時出現")
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+        print("applicationWillTerminate將程式完全關閉結束時出現")
     }
 
 
--- ShowActiveInLabel/ViewController.swift
+++ ShowActiveInLabel/ViewController.swift
@@ -9,15 +9,62 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var activeNum = 0
+    
+    @IBOutlet weak var showActiveLabel: UILabel!
+    
+    @IBOutlet weak var showActiveText: UILabel!
+    func sayGoodBye(noti:Notification) {
+        
+        activeNum -= 1
+        showActiveLabel.text = "\(activeNum)"
+        showActiveText.text = "現在是app進入背景時的狀態，會將上列數字-1"
+        print("sayGoodBye from \(noti)")
+        
+    }
+    
+    func sayHi(noti:Notification) {
+        
+        activeNum += 2
+        showActiveLabel.text = "\(activeNum)"
+        showActiveText.text = "現在是app由背景回到前景時的狀態，會將上列數字+2"
+        
+        print("sayHi from \(noti)")
+        
+    }
+    
+    func addObserver(_ observer: Any, selector aSelector:
+        Selector, name aName: NSNotification.Name?, object
+        anObject: Any?) {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        showActiveLabel.text = "\(activeNum)"
+        
+        showActiveText.text = "現在是剛進入app時的狀態，預設值為0"
+        
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(ViewController.sayGoodBye(noti:)), name:
+            Notification.Name.UIApplicationDidEnterBackground, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector:
+            #selector(ViewController.sayHi(noti:)), name:
+            Notification.Name.UIApplicationWillEnterForeground, object: nil)
+        
+        print("viewDidLoad")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        print("didReceiveMemoryWarning")
     }
 
 
